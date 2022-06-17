bf609fb96b69b18bd4c3620c383787e96bc6a069
Erico Vasconcelos <erico@ettbrasil.com.br>
2022-06-17 18:15:22 -0300

    [ADD] Add test config

---
+++ src/__tests__/screens/profile.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import { Profile } from '../../screens/Profile';
+
+test('Check if button placeholder shows correctly', () => {
+    const { getByPlaceholderText } = render(<Profile />);
+    const inputName = getByPlaceholderText('Nome');
+    
+    expect(inputName.props.placeholder).toBeTruthy();
+});
